0c6d96e7edb464906438ca94c96f554ec47edcca
unknown <khalil.meziou@telecom-sudparis.eu>
2021-05-30 15:55:38 +0200

    Premier squelette

---
+++ src/myMain.cpp
@@ -0,0 +1,12 @@
+#include "myMain.h"
+#include <pugixml.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include <iostream>
+
+using namespace std;
+
+int myMain()
+{
+    return 0;
+}
---
+++ unitTests.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+using namespace std;
+TEST(TestReadXML, TestCircle) {
+	EXPECT_EQ(1, 1);
+}
