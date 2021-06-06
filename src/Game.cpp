cc26de26d08cbb9dac4b140ee2b7ba790a907728
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-06-06 13:50:36 +0200

    Implémentation des méthodes de la classe Game

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,9 +1,17 @@
 #include "Game.h"
+#include "Menu.h"
 
 void Game::init() {
 
 	/* Créer les niveaux, les menus, et les ajouter au jeu ici. */
 
+	/* Ne pas oublier d'ajouter l'écran qui s'affiche (écran d'erreur) quand un écran particulier n'est pas trouvé. */
+	/* Cet écran doit être associé à Menu::NOT_FOUND dans Game::menus.                                              */
+
+	auto errorScreen = std::make_unique<Screen>();
+	// Ajouter les éléments de l'écran d'erreur ici.
+	menus[Menu::NOT_FOUND] = move(errorScreen);
+	
 }
 
 Screen* Game::getMenu(int id) const { 
@@ -12,8 +20,34 @@ Screen* Game::getMenu(int id) const {
 
 	if (entry != menus.end()) {
 
-		// TODO
+		return entry->second.get();
 
 	}
+
+	return menus.find(Menu::NOT_FOUND)->second.get();
+
+}
+
+Screen* Game::getLevel(int id) const {
+
+	if (id < 0 || id >= menus.size()) {
+
+		return menus.find(Menu::NOT_FOUND)->second.get();
+
+	}
+
+	return levels[id].get();
+
+}
+
+void Game::addMenu(std::unique_ptr<Screen> menu) {
+
+	levels.push_back(move(menu));
+
+}
+
+void Game::addLevel(std::unique_ptr<Screen> level) {
+
+	levels.push_back(move(level));
 
 }
--- src/Menu.h
+++ src/Menu.h
@@ -1,3 +1,3 @@
 #pragma once
 
-enum Menu {WIN, LOSE, LAUNCH};
+enum Menu {WIN, LOSE, LAUNCH, NOT_FOUND};
