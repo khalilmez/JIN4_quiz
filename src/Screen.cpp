200e3c049a25d15b1424ed5a41d0bb1cba1e1655
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-06-06 14:23:30 +0200

    Implémentation des méthodes de la classe Screen

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -1,3 +1,27 @@
-
 #include "Screen.h"
 
+void Screen::render(sf::RenderWindow &window) const {
+
+	window.clear(backgroundColor);
+
+	for (auto &element : elements) {
+
+		element.render(window);
+
+	}
+
+	window.display();
+
+}
+
+void Screen::handleEvent(sf::Event event) {
+
+	eventHandler.handle(this, event);
+
+}
+
+Screen* Screen::update() {
+
+	updateStrategy.update(this);
+
+}
